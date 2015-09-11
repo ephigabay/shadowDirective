403933ccf2a9a6904fa5cf661159fb88fd385d7f
Ephi Gabay <gabay.ephi@gmail.com>
2015-09-11 14:55:33 +0300

    made some improvements

--- src/shadowDirective.js
+++ src/shadowDirective.js
@@ -8,24 +8,35 @@
     };
 
     angular.module('shadowDirective', [])
-        .config(function ($compileProvider) {
+        .config(['$compileProvider', function ($compileProvider) {
             angular.registerDirective = $compileProvider.directive;
-        })
-        .run(function ($injector, $compile) {
+        }])
+        .run(['$injector', '$compile', '$templateRequest', function ($injector, $compile, $templateRequest) {
             stuffToRun.forEach(function (itemToRun) {
                 var directive = $injector.invoke(itemToRun.directiveProvider);
                 directive.oldLink = directive.link;
+                var styleUrl = directive.styleUrl;
                 directive.link = function (scope, element, attrs, ctrl) {
                     var shadowParent = element[0].createShadowRoot();
+                    if(styleUrl) {
+                        $templateRequest(styleUrl).then(function(content) {
+                            var styleTag = document.createElement("style");
+                            styleTag.innerText = content;
+                            shadowParent.appendChild(styleTag);
+                        })
+                    }
+                    element.find('[ng-transclude]')[0].removeAttribute('ng-transclude');
                     var compiled = $compile(element.children())(scope);
                     angular.forEach(compiled, function (compiledItem) {
                         shadowParent.appendChild(compiledItem);
                     });
-                    directive.oldLink.apply(directive, arguments);
+                    if(directive.oldLink) {
+                        directive.oldLink.apply(directive, arguments);
+                    }
                 };
                 angular.registerDirective(itemToRun.directiveName, function () {
                     return directive;
                 })
             });
-        });
+        }]);
 })();
